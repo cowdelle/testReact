21d31c08feb6efc91c8640a2cdbe77bc0b7efab4
cowdelle <elleycowdell@gmail.com>
2024-03-08 13:19:46 -0700

    final basketball assignment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import TeamList from "./components/TeamCard";
+import React from "react";
+import basketballTeamsData from "./CollegeBasketballTeams.json";
+import "./App.css"; // Import CSS file
 
-const App = () => {
-  const [teams, setTeams] = useState([]);
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
 
-  useEffect(() => {
-    fetch("/CollegeBasketballTeams.json")
-      .then((response) => response.json())
-      .then((data) => setTeams(data))
-      .catch((error) => console.error("Error fetching teams:", error));
-  }, []);
+const basketballTeams: TeamProps[] = basketballTeamsData.teams;
 
+function Welcome() {
+  return <h1>Basketball Teams List</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="card">
+        <h3>Name: {oneTeam.school}</h3>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
   return (
-    <div className="App">
-      <Header />
-      <TeamList team={teams} />
+    <div>
+      {basketballTeams.map((oneTeam, index) => (
+        <Team key={index} {...oneTeam} />
+      ))}
     </div>
   );
-};
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <TeamList />
+    </div>
+  );
+}
 
 export default App;
